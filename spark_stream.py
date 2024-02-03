fc951dbcf96c089c10e30673c0ec4d1e40173810
shrria <58872002+shrria@users.noreply.github.com>
2024-02-04 02:14:04 +0700

    🔨 Add code of spark and docker images

---
+++ spark_stream.py
@@ -0,0 +1,204 @@
+import logging
+
+from cassandra.cluster import Cluster
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, from_json
+from pyspark.sql.types import StructType, StructField, StringType
+
+
+def create_keyspace(session):
+    session.execute(
+        """
+        CREATE KEYSPACE IF NOT EXISTS spark_streams
+        WITH replication = {'class': 'SimpleStrategy', 'replication_factor': 1};
+        """
+    )
+
+    print("Keyspace created successfully")
+
+
+def create_table(session):
+    session.execute(
+        """
+        CREATE TABLE IF NOT EXISTS spark_streams.users (
+            id TEXT PRIMARY KEY,
+            first_name TEXT,
+            last_name TEXT,
+            gender TEXT,
+            address TEXT,
+            postcode TEXT,
+            email TEXT,
+            username TEXT,
+            dob TEXT,
+            registered_date TEXT,
+            phone TEXT,
+            picture TEXT
+            );
+        """
+    )
+
+
+def insert_data(session, **kwargs):
+    print("inserting data...")
+
+    user_id = kwargs.get("id")
+    first_name = kwargs.get("first_name")
+    last_name = kwargs.get("last_name")
+    gender = kwargs.get("gender")
+    address = kwargs.get("address")
+    postcode = kwargs.get("postcode")
+    email = kwargs.get("email")
+    username = kwargs.get("username")
+    dob = kwargs.get("dob")
+    registered_date = kwargs.get("registered_date")
+    phone = kwargs.get("phone")
+    picture = kwargs.get("picture")
+
+    try:
+        session.execute(
+            """
+            INSERT INTO spark_streams.users (id, first_name, last_name, gender, address,
+            postcode, email, username, dob, registered_date, phone, picture)
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+            """,
+            (
+                user_id,
+                first_name,
+                last_name,
+                gender,
+                address,
+                postcode,
+                email,
+                username,
+                dob,
+                registered_date,
+                phone,
+                picture,
+            ),
+        )
+        logging.info("Data inserted for {first_name} {last_name}")
+
+    except Exception as e:
+        logging.error(f"Could not insert data: {e}")
+
+
+def create_spark_connection():
+    s_conn = None
+
+    try:
+        s_conn = (
+            SparkSession.builder.appName("SparkDataStreaming")
+            .config(
+                "spark.jars.packages",
+                "com.datastax.spark:spark-cassandra-connector_2.13:3.4.1,"
+                "org.apache.spark:spark-sql-kafka-0-10_2.13:3.5.0",
+            )
+            .config("spark.cassandra.connection.host", "localhost")
+            .getOrCreate()
+        )
+
+        s_conn.sparkContext.setLogLevel("ERROR")
+        logging.info("Spark connection created successfully")
+    except Exception as e:
+        logging.error(f"Could not create spark connection: {e}")
+
+    return s_conn
+
+
+def connect_to_kafka(spark_conn):
+    spark_df = None
+
+    kafka_server = "localhost:9092"
+    kafka_topic = "users_created"
+
+    try:
+        spark_df = (
+            spark_conn.readStream.format("kafka")
+            .option("kafka.bootstrap.servers", kafka_server)
+            .option("subscribe", kafka_topic)
+            .option("startingOffsets", "earliest")
+            .load()
+        )
+        logging.info("kafka dataframe created successfully")
+
+    except Exception as e:
+        logging.error(f"kafka dataframe could not be created: {e}")
+
+    return spark_df
+
+
+def create_cassandra_connection():
+    cas_session = None
+
+    try:
+        cluster = Cluster(["localhost"])
+
+        cas_session = cluster.connect()
+    except Exception as e:
+        logging.error(f"Could not create cassandra connection: {e}")
+
+    return cas_session
+
+
+def create_selection_df_from_kafka(spark_df):
+    schema = StructType(
+        [
+            StructField("id", StringType(), False),
+            StructField("first_name", StringType(), False),
+            StructField("last_name", StringType(), False),
+            StructField("gender", StringType(), False),
+            StructField("address", StringType(), False),
+            StructField("postcode", StringType(), False),
+            StructField("email", StringType(), False),
+            StructField("username", StringType(), False),
+            StructField("dob", StringType(), False),
+            StructField("registered_date", StringType(), False),
+            StructField("phone", StringType(), False),
+            StructField("picture", StringType(), False),
+        ]
+    )
+
+    sel = (
+        spark_df.selectExpr("CAST(value AS STRING)")
+        .select(from_json(col("value"), schema).alias("data"))
+        .select("data.*")
+    )
+    print(sel)
+
+
+if __name__ == "__main__":
+    ## create spark connection
+    spark_conn = create_spark_connection()
+
+    if spark_conn is None:
+        logging.error("Spark connection could not be created")
+        exit(1)
+
+    ## connect to kafka with spark connection
+    spark_df = connect_to_kafka(spark_conn)
+    if spark_df is None:
+        logging.error("Kafka connection could not be created")
+        exit(1)
+
+    selection_df = create_selection_df_from_kafka(spark_df)
+    session = create_cassandra_connection()
+
+    if session is None:
+        logging.error("Cassandra connection could not be created")
+        exit(1)
+
+    create_keyspace(session)
+    create_table(session)
+
+    # start streaming
+    logging.info("Streaming is being started...")
+
+    streaming_query = (
+        selection_df.writeStream.format("org.apache.spark.sql.cassandra")
+        .option("checkpointLocation", "/tmp/checkpoint")
+        .option("keyspace", "spark_streams")
+        .option("table", "users")
+        .start()
+    )
+
+    streaming_query.awaitTermination()
